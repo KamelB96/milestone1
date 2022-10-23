952e887e1f9d66ef624e05ab26ef74921e16ffa4
kamel Barakat <kamelbarakat96@yahoo.com>
2022-10-22 22:42:23 -0400

    Reset button works. Now working on players turns

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 //set up reset game button
-
+document.getElementById("resetGame").onclick = function() {
+    document.getElementById("box1").innerHTML = "";
+    document.getElementById("box2").innerHTML = "";
+    document.getElementById("box3").innerHTML = "";
+    document.getElementById("box4").innerHTML = "";
+    document.getElementById("box5").innerHTML = "";
+    document.getElementById("box6").innerHTML = "";
+    document.getElementById("box7").innerHTML = "";
+    document.getElementById("box8").innerHTML = "";
+    document.getElementById("box9").innerHTML = "";
+  };
 
 //set up reset score button
 document.getElementById("resetScore").onclick = function() {
@@ -9,66 +19,74 @@ document.getElementById("resetScore").onclick = function() {
   
   };
 
-//set up X O trurn system
+//How to win
+
+
+  //set up X O trurn system
 
 //set up buttons 1-9 
 
 if (turn= 'PLAYER 1s TURN') {
-    document.getElementById('btn1').onclick = function(){
-        document.getElementById("box1").innerHTML = "X";    
+    document.getElementById('box1').onclick = function(){
+        document.getElementById("box1").innerHTML = "X";  
+        document.getElementById("turn").innerText ='PLAYER 2s TURN' ;
     }
-    document.getElementById('btn2').onclick = function(){
+    document.getElementById('box2').onclick = function(){
         document.getElementById("box2").innerHTML = "X";    
     }
-    document.getElementById('btn3').onclick = function(){
+    document.getElementById('box3').onclick = function(){
         document.getElementById("box3").innerHTML = "X";    
     }
-    document.getElementById('btn4').onclick = function(){
+    document.getElementById('box4').onclick = function(){
         document.getElementById("box4").innerHTML = "X";    
     }
-    document.getElementById('btn5').onclick = function(){
+    document.getElementById('box5').onclick = function(){
         document.getElementById("box5").innerHTML = "X";    
     }
-    document.getElementById('btn6').onclick = function(){
+    document.getElementById('box6').onclick = function(){
         document.getElementById("box6").innerHTML = "X";    
     }
-    document.getElementById('btn7').onclick = function(){
+    document.getElementById('box7').onclick = function(){
         document.getElementById("box7").innerHTML = "X";    
     }
-    document.getElementById('btn8').onclick = function(){
+    document.getElementById('box8').onclick = function(){
         document.getElementById("box8").innerHTML = "X";    
     }
-    document.getElementById('btn9').onclick = function(){
+    document.getElementById('box9').onclick = function(){
         document.getElementById("box9").innerHTML = "X";  
 }
 }
 
-else if (turn= 'PLAYER 2s TURN') {
-    document.getElementById('btn1').onclick = function(){
-        document.getElementById("box1").innerHTML = "O";    
+ else if (turn= 'PLAYER 2s TURN') {
+    document.getElementById('box1').onclick = function(){
+        document.getElementById("box1").innerHTML = "O";  
+        document.getElementById("turn").innerText ='PLAYER 2s TURN' ;
     }
-    document.getElementById('btn2').onclick = function(){
+    document.getElementById('box2').onclick = function(){
         document.getElementById("box2").innerHTML = "O";    
     }
-    document.getElementById('btn3').onclick = function(){
+    document.getElementById('box3').onclick = function(){
         document.getElementById("box3").innerHTML = "O";    
     }
-    document.getElementById('btn4').onclick = function(){
+    document.getElementById('box4').onclick = function(){
         document.getElementById("box4").innerHTML = "O";    
     }
-    document.getElementById('btn5').onclick = function(){
+    document.getElementById('box5').onclick = function(){
         document.getElementById("box5").innerHTML = "O";    
     }
-    document.getElementById('btn6').onclick = function(){
+    document.getElementById('box6').onclick = function(){
         document.getElementById("box6").innerHTML = "O";    
     }
-    document.getElementById('btn7').onclick = function(){
+    document.getElementById('box7').onclick = function(){
         document.getElementById("box7").innerHTML = "O";    
     }
-    document.getElementById('btn8').onclick = function(){
+    document.getElementById('box8').onclick = function(){
         document.getElementById("box8").innerHTML = "O";    
     }
-    document.getElementById('btn9').onclick = function(){
-        document.getElementById("box9").innerHTML = "O";  
+    document.getElementById('box9').onclick = function(){
+        document.getElementById("box9").innerHTML = "O"; 
 }
+}
+if (box1 = 'X') {
+    console.log ("winner")
 }
